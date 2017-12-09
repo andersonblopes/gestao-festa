ae0868f0593c2c87977ab4c032414bc7524d465a
Anderson Lopes <anderson.lopes.2010@gmail.com>
2017-12-09 15:13:06 -0300

    Send objects in view

--- src/main/java/com/lopes/gestaofesta/controller/ConvidadosController.java
+++ src/main/java/com/lopes/gestaofesta/controller/ConvidadosController.java
@@ -1,14 +1,25 @@
 package com.lopes.gestaofesta.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.lopes.gestaofesta.repository.ConvidadosRepository;
 
 @Controller
 public class ConvidadosController {
 
+	@Autowired
+	private ConvidadosRepository convidadosRepository;
+
 	@GetMapping("/convidados")
-	public String listar() {
-		return "ListaConvidados";
+	public ModelAndView listar() {
+		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
+
+		modelAndView.addObject("convidados", convidadosRepository.findAll());
+
+		return modelAndView;
 	}
 
 }
