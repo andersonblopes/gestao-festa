d583e2e83b8460000fbf04b2cdcfd852ce746684
Anderson Lopes <anderson.lopes.2010@gmail.com>
2017-12-09 15:41:00 -0300

    Fix import libs

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,4 +2,4 @@ insert into convidado (id, nome, quantidade_acompanhantes) values (1, 'Pedro', 2
 insert into convidado (id, nome, quantidade_acompanhantes) values (2, 'Maria', 3);
 insert into convidado (id, nome, quantidade_acompanhantes) values (3, 'Ricardo', 1);
 insert into convidado (id, nome, quantidade_acompanhantes) values (4, 'Ana Beatriz', 7);
-insert into convidado (id, nome, quantidade_acompanhantes) values (5, 'helena', 4);
+insert into convidado (id, nome, quantidade_acompanhantes) values (5, 'Helena', 4);
