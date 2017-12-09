d76d22a85a0dec467f5aab2eb4da17f6a24e4256
Anderson Lopes <anderson.lopes.2010@gmail.com>
2017-12-09 15:56:44 -0300

    Included 'convidado' in database by view

--- src/main/java/com/lopes/gestaofesta/controller/ConvidadosController.java
+++ src/main/java/com/lopes/gestaofesta/controller/ConvidadosController.java
@@ -3,23 +3,34 @@ package com.lopes.gestaofesta.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.lopes.gestaofesta.model.Convidado;
 import com.lopes.gestaofesta.repository.ConvidadosRepository;
 
 @Controller
+@RequestMapping("/convidados")
 public class ConvidadosController {
 
 	@Autowired
 	private ConvidadosRepository convidadosRepository;
 
-	@GetMapping("/convidados")
+	@GetMapping
 	public ModelAndView listar() {
 		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
 
+		modelAndView.addObject(new Convidado());
 		modelAndView.addObject("convidados", convidadosRepository.findAll());
 
 		return modelAndView;
+	}
+
+	@PostMapping
+	public String salvar(Convidado convidado) {
+		this.convidadosRepository.save(convidado);
+		return "redirect:/convidados";
 	}
 
 }
