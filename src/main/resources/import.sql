5f17310b81c7ca154abe43c5304403c7cf41f449
Anderson Lopes <anderson.lopes.2010@gmail.com>
2017-12-09 15:33:07 -0300

    Show data in view

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,5 @@
 insert into convidado (id, nome, quantidade_acompanhantes) values (1, 'Pedro', 2);
 insert into convidado (id, nome, quantidade_acompanhantes) values (2, 'Maria', 3);
 insert into convidado (id, nome, quantidade_acompanhantes) values (3, 'Ricardo', 1);
+insert into convidado (id, nome, quantidade_acompanhantes) values (4, 'Ana Beatriz', 7);
+insert into convidado (id, nome, quantidade_acompanhantes) values (5, 'helena', 4);
