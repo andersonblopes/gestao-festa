4696cfa06cd60f39be2bcc9b913b1664ccd73fc1
Anderson Lopes <anderson.lopes.2010@gmail.com>
2017-12-09 19:54:58 -0300

    Alter security

--- src/main/java/com/lopes/gestaofesta/config/InMemorySecurityConfig.java
+++ src/main/java/com/lopes/gestaofesta/config/InMemorySecurityConfig.java
@@ -3,18 +3,16 @@ package com.lopes.gestaofesta.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
+@EnableWebSecurity
 public class InMemorySecurityConfig {
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
-		builder.inMemoryAuthentication()
-		.withUser("Anderson").password("123").roles("USER")
-		.and()
-		.withUser("Helena").password("123").roles("USER")
-		.and()
-		.withUser("Elaine").password("123").roles("USER");
+		builder.inMemoryAuthentication().withUser("Anderson").password("123").roles("USER").and().withUser("Helena")
+				.password("123").roles("USER").and().withUser("Elaine").password("123").roles("USER");
 	}
 
 }
